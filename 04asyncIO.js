49bd317378558546440301f51f3685478bdcb4e6
Emma Bishop <emma.b.bishop@gmail.com>
2015-12-13 10:17:06 -0500

    Cleaned up 04async.js

--- 04asyncIO.js
+++ 04asyncIO.js
@@ -1,8 +1,7 @@
 var fs = require('fs');
 var filePath = process.argv[2];
 
-var counter = 0;
-var contents =  fs.readFile(filePath, 'utf8', function(err, data){
+fs.readFile(filePath, 'utf8', function(err, data){
 	if (err) {throw err};
 	var lineBreaks = data.match(/\n/g).length;
 	console.log(lineBreaks);
